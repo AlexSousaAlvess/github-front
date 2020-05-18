f1dab49bd8e54b04e161b1e25e67ce9c225f78eb
alex sousa <alexsousaalves317@gmail.com>
2020-05-18 01:18:01 -0300

    completed component header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 
 import { Div } from './styles';
+import {FiGithub} from 'react-icons/fi';
 
 class Header extends Component{
   render(){
     return(
       <Div>
+        <FiGithub size={30}/>
+        <p>Github app</p>
       </Div>
     )
   }
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -4,9 +4,25 @@ import styled from 'styled-components';
 
 export const Div = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  
   width: 100%;
-  height: 50px;
+  height: 60px;
   margin: 0;
+  padding: 0;
   background-color: #24292e;
+
+  svg {
+    color: #FFF;
+    margin: 0 10px;
+  }
+
+  P {
+    color: #FFF;
+    font-weight: bold;
+    font-size: 18px;
+    margin: 0 10px;
+  }
 `;
 
