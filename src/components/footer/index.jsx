d2fa2c5465573b4c936513c6ae007da6c5e31f1d
alex sousa <alexsousaalves317@gmail.com>
2020-05-18 01:25:55 -0300

    created component footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+// import {} from './styles';
+
+class Footer extends Component{
+  render(){
+    return(
+      <h1>Eu sou um footer</h1>
+    )
+  }
+}
+
+export default Footer;
