96a97aaae90953c2e1409437ca591d2dbc3d253a
alex sousa <alexsousaalves317@gmail.com>
2020-05-18 00:43:09 -0300

    create component Header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import { Div } from './styles';
+
+class Header extends Component{
+  render(){
+    return(
+      <Div>
+      </Div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/components/header/styles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  margin: 0;
+  background-color: #24292e;
+`;
+
